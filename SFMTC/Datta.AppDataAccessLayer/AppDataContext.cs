f2454b87770d97105c39d9a3440d4f0f87677450
Manuel Peise <manuel.p80@gmx.de>
2022-12-24 10:13:09 +0100

    add-AppDataContext-recipe-table
    affected items: #06

---
+++ SFMTC/Datta.AppDataAccessLayer/AppDataContext.cs
@@ -0,0 +1,14 @@
+﻿using Datta.AppDataAccessLayer.Entities.CookingBook;
+using Microsoft.EntityFrameworkCore;
+
+namespace Datta.AppDataAccessLayer
+{
+    public class AppDataContext: DbContext
+    {
+        public DbSet<Recipe> Recipes { get; set; }
+        public AppDataContext(DbContextOptions<AppDataContext> options):base(options)
+        {
+
+        }
+    }
+}
---
+++ SFMTC/Datta.AppDataAccessLayer/Entities/CookingBook/Recipe.cs
@@ -0,0 +1,8 @@
+﻿using Shared.Data.Models.Cookingbook;
+
+namespace Datta.AppDataAccessLayer.Entities.CookingBook
+{
+    public class Recipe: ARecipe
+    {
+    }
+}
